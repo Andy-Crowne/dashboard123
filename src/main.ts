92ccf9c94e6b0ba56cd4710fd485c7acd028d9f6
Andy-Crowne <izocougar@gmail.com>
2023-03-17 14:56:55 +0300

    ROMA

---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { Container, ContainerModule, interfaces } from 'inversify';
+import { App } from './app';
+import { ExeptionFilter } from './errors/exeption.filter';
+import { IExeptionFilter } from './errors/exeption.filter.interface';
+import { ILogger } from './logger/logger.interface';
+import { LoggerService } from './logger/loggers.service';
+import { TYPES } from './types';
+import { UserContorller } from './users/users.controller';
+
+export interface IBootstrapReturn {
+	appContainer: Container;
+	app: App;
+}
+
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+	bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+	bind<UserContorller>(TYPES.UserController).to(UserContorller);
+	bind<App>(TYPES.Application).to(App);
+});
+
+function bootstrap(): IBootstrapReturn {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	app.init();
+	return { appContainer, app };
+}
+
+export const { app, appContainer } = bootstrap();
